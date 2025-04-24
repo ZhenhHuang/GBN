3891e9d2b8812c940d2f3961c0585e13f82b390e
ZhenhaoHuang <127167919+ZhenhHuang@users.noreply.github.com>
2025-04-24 12:36:03 +0800

    Update models.py

--- modules/models.py
+++ modules/models.py
@@ -9,15 +9,19 @@ EPS = 1e-4
 
 
 class BoundaryConvLayer(nn.Module):
-    def __init__(self, in_dim, out_dim, bias, act=None, drop=0.3):
+    def __init__(self, in_dim, hid_dim, out_dim, bias, act=None, drop=0.3):
         super().__init__()
         self.fc = nn.Linear(in_dim, out_dim, bias=bias)
         self.act = act if act is not None else nn.Identity()
-        self.rate = nn.Linear(in_dim, out_dim, bias=False)
-        self.rob_bound = nn.Sequential(nn.Linear(in_dim, 512, bias=True),
+        self.rate = nn.Sequential(nn.Linear(in_dim, out_dim, bias=False),
+                                  nn.Softplus(),
+                                  nn.Dropout(drop))
+        self.rob_bound = nn.Sequential(nn.Linear(in_dim, hid_dim, bias=True),
                                        nn.Softplus(),
-                                       nn.Linear(512, out_dim, bias=True))
-        self.norm = GraphNorm(out_dim)
+                                       nn.Linear(hid_dim, out_dim, bias=True),
+                                       nn.LayerNorm(out_dim),
+                                       nn.Dropout(drop))
+        self.norm = nn.LayerNorm(out_dim)
         self.drop = nn.Dropout(drop)
 
     def forward(self, x, edge_index, degree):
@@ -26,27 +30,27 @@ class BoundaryConvLayer(nn.Module):
         edge_index: 2 * E
         degrees: N
         """
-        rate = self.drop(F.softplus(self.rate(x))) + EPS
-        gamma = self.drop(self.rob_bound(x))
+        rate = self.rate(x)
+        gamma = self.rob_bound(x)
         x = self.fc(x)
         z = x
         x = self.drop(x)
         row, col = edge_index[0], edge_index[1]
         x = x[row] + x[col]
         x = scatter_sum(x, row, dim=0)
-        x = (rate * x + gamma) / (1 + rate * degree.unsqueeze(1) + EPS) + z
+        x = (rate * x + gamma) / (1 + rate * degree.unsqueeze(1) + EPS) - z
         x = self.norm(x)
         return self.drop(self.act(x))
 
 
 class BoundaryGCN(nn.Module):
-    def __init__(self, n_layers, in_dim, embed_dim, out_dim, bias, act, drop=0.3):
+    def __init__(self, n_layers, in_dim, hid_dim, embed_dim, out_dim, bias, act, drop=0.3):
         super().__init__()
-        self.layers = nn.ModuleList([BoundaryConvLayer(in_dim, embed_dim, bias, act, drop)])
+        self.layers = nn.ModuleList([BoundaryConvLayer(in_dim, hid_dim, embed_dim, bias, act, drop)])
         self.res_lin = nn.ModuleList([nn.Linear(in_dim, embed_dim, bias=False) for _ in range(n_layers - 1)])
         for _ in range(n_layers - 2):
-            self.layers.append(BoundaryConvLayer(embed_dim, embed_dim, bias, act, drop))
-        self.layers.append(BoundaryConvLayer(embed_dim, out_dim, bias, None))
+            self.layers.append(BoundaryConvLayer(embed_dim, hid_dim, embed_dim, bias, act, drop))
+        self.layers.append(BoundaryConvLayer(embed_dim, hid_dim, out_dim, bias, None))
 
     def forward(self, data):
         x = data.x
@@ -57,4 +61,3 @@ class BoundaryGCN(nn.Module):
             x = layer(x, edge_index, degree) + res(z)
         x = self.layers[-1](x, edge_index, degree)
         return x
-
