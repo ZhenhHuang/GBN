11b8f7c4d6ef6745e859a4363a927c79393efab5
ZhenhaoHuang <127167919+ZhenhHuang@users.noreply.github.com>
2025-04-24 12:20:04 +0800

    Update data_utils.py

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -7,14 +7,6 @@ import warnings
 warnings.filterwarnings('ignore')
 
 
-input_dim_dict = {"KarateClub": 34, "Cora": 1433, "Citeseer": 3703, "PubMed": 500,
-                  'ogbn-arxiv': 128, "CS": 6805, "GitHub": 128, "USA": 1190, "computers": 767, "Flickr": 500,
-                  "WikiCS": 128, "COLLAB": 128, "Texas": 1703, "Cornell": 1703, "Wisconsin": 1703}
-class_num_dict = {"KarateClub": 4, "Cora": 7, "Citeseer": 6, "PubMed": 3, "ogbn-arxiv": 40, "CS": 15,
-                  "GitHub": 2, "USA": 4, "computers": 10, "Flickr": 7, "WikiCS": 10, "COLLAB": 3,
-                  "Texas": 5, "Cornell": 5, "Wisconsin": 5}
-
-
 def load_data(root: str, data_name: str,
               num_val=0.1, num_test=0.2, num_per_class=None):
     if num_per_class is None:
