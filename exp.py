42935a807eabf24ec42ed0d7de5f8f59ad3903b2
ZhenhaoHuang <127167919+ZhenhHuang@users.noreply.github.com>
2025-04-30 21:24:28 +0800

    Update exp.py

--- exp.py
+++ exp.py
@@ -81,7 +81,7 @@ class Exp:
             total_test_weighted_f1.append(weighted_f1)
             total_test_macro_f1.append(macro_f1)
         mean, std = np.mean(total_test_acc), np.std(total_test_acc)
-        self.logger.info(f"Best ACCs:" + f" {acc * 100: .2f}%" for acc in total_test_acc)
+        self.logger.info(f"Best ACCs: {[round(acc * 100, 2) for acc in total_test_acc]}")
         self.logger.info(f"Evaluation Acc is {mean * 100: .2f}% \u00B1 {std * 100: .2f}%")
         mean, std = np.mean(total_test_weighted_f1), np.std(total_test_weighted_f1)
         self.logger.info(f"Evaluation weighted F1 is {mean * 100: .2f}% \u00B1 {std * 100: .2f}%")
